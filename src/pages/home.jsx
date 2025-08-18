8b4357105cd2996eccce22267106775f29e2f3fe
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-08-18 17:08:26 +0200

    add pages

---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Home() {
+    const x = 5;
+
+    return <div>The value of x is {x}</div>;
+}
+
+export default Home;
---
+++ src/services/foodsApi.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://www.themealdb.com/api/json/v1/1',
+});
+
+export async function foodsApi() {
+  const response = await client.get('/filter.php?c=Seafood');
+  return response.data.meals;
+}
+
